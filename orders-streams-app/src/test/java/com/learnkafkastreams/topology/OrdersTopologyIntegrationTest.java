2d1c48c7f37057c04db3b1ceebf1c871675afe37
lucas.saldanha <lk.saldanha@gmail.com>
2024-05-28 20:06:34 +0200

    test: integration tests for orders count and revenue

--- orders-streams-app/src/test/java/com/learnkafkastreams/topology/OrdersTopologyIntegrationTest.java
+++ orders-streams-app/src/test/java/com/learnkafkastreams/topology/OrdersTopologyIntegrationTest.java
@@ -1,102 +1,153 @@
 package com.learnkafkastreams.topology;
 
+import static com.learnkafkastreams.topology.OrdersTopology.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.learnkafkastreams.domain.Order;
 import com.learnkafkastreams.domain.OrderLineItem;
 import com.learnkafkastreams.domain.OrderType;
 import com.learnkafkastreams.service.OrderService;
-import org.apache.kafka.streams.KafkaStreams;
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
 import org.apache.kafka.streams.KeyValue;
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.config.StreamsBuilderFactoryBean;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 
-import java.math.BigDecimal;
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.List;
-import java.util.Objects;
-
-import static com.learnkafkastreams.topology.OrdersTopology.*;
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-
 @SpringBootTest
-public class OrdersTopologyIntegrationTest {
+@EmbeddedKafka(topics = {ORDERS, STORES})
+@TestPropertySource(
+    properties = {
+      "spring.kafka.streams.bootstrap-servers = ${spring.embedded.kafka.brokers}",
+      "spring.kafka.producer.bootstrap-servers = ${spring.embedded.kafka.brokers}"
+    })
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class OrdersTopologyIntegrationTest {
 
-    @Autowired
-    KafkaTemplate<String, String> kafkaTemplate;
+  @Autowired
+  KafkaTemplate<String, String> kafkaTemplate;
+  @Autowired
+  StreamsBuilderFactoryBean streamsBuilderFactoryBean;
+  @Autowired
+  ObjectMapper objectMapper;
+  @Autowired
+  OrderService orderService;
 
-    @Autowired
-    StreamsBuilderFactoryBean streamsBuilderFactoryBean;
-    @Autowired
-    ObjectMapper objectMapper;
+  @BeforeEach
+  void setup() {
+  }
 
+  @AfterEach
+  void destroy() {
+    Objects.requireNonNull(streamsBuilderFactoryBean.getKafkaStreams()).close();
+    streamsBuilderFactoryBean.getKafkaStreams().cleanUp();
+  }
 
-    private void publishOrders() {
-        orders()
-                .forEach(order -> {
-                    String orderJSON = null;
-                    try {
-                        orderJSON = objectMapper.writeValueAsString(order.value);
-                    } catch (JsonProcessingException e) {
-                        throw new RuntimeException(e);
-                    }
-                    kafkaTemplate.send(ORDERS, order.key, orderJSON);
-                });
+  @Test
+  void ordersCount() {
+    // Given
+    publishOrders();
+    // When
 
+    // Then
+    // assert orderService.getOrdersCount(GENERAL_ORDERS).size() == 1;
+    Awaitility.await().atMost(Duration.ofSeconds(10))
+            .pollDelay(Duration.ofSeconds(1))
+            .ignoreExceptions()
+            .until(() -> orderService.getOrdersCount(GENERAL_ORDERS).size(), equalTo(1));
 
-    }
+    var generalOrdersCount = orderService.getOrdersCount(GENERAL_ORDERS);
+    assertEquals(1, generalOrdersCount.getFirst().orderCount());
+  }
 
+  @Test
+  void ordersRevenue() {
+    // Given
+    publishOrders();
+    // Then
+    // assert orderService.getOrdersCount(GENERAL_ORDERS).size() == 1;
+    Awaitility.await().atMost(Duration.ofSeconds(10))
+            .pollDelay(Duration.ofSeconds(1))
+            .ignoreExceptions()
+            .until(() -> orderService.getOrdersCount(GENERAL_ORDERS).size(), equalTo(1));
 
-    static List<KeyValue<String, Order>> orders() {
+    Awaitility.await().atMost(Duration.ofSeconds(10))
+            .pollDelay(Duration.ofSeconds(1))
+            .ignoreExceptions()
+            .until(() -> orderService.getOrdersCount(RESTAURANT_ORDERS).size(), equalTo(1));
 
-        var orderItems = List.of(
-                new OrderLineItem("Bananas", 2, new BigDecimal("2.00")),
-                new OrderLineItem("Iphone Charger", 1, new BigDecimal("25.00"))
-        );
+    var generalOrdersRevenue = orderService.getRevenueByOrderType(GENERAL_ORDERS);
+    assertEquals(new BigDecimal("27.00"), generalOrdersRevenue.getFirst().totalRevenue().runningRevenue());
 
-        var orderItemsRestaurant = List.of(
-                new OrderLineItem("Pizza", 2, new BigDecimal("12.00")),
-                new OrderLineItem("Coffee", 1, new BigDecimal("3.00"))
-        );
+    var restaurantOrdersRevenue = orderService.getRevenueByOrderType(RESTAURANT_ORDERS);
+    assertEquals(new BigDecimal("15.00"), restaurantOrdersRevenue.getFirst().totalRevenue().runningRevenue());
+  }
 
-        var order1 = new Order(12345, "store_1234",
-                new BigDecimal("27.00"),
-                OrderType.GENERAL,
-                orderItems,
-                //LocalDateTime.now()
-                LocalDateTime.parse("2023-02-21T21:25:01")
-        );
+  private void publishOrders() {
+    orders()
+        .forEach(
+            order -> {
+              String orderJSON = null;
+              try {
+                orderJSON = objectMapper.writeValueAsString(order.value);
+              } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+              }
+              kafkaTemplate.send(ORDERS, order.key, orderJSON);
+            });
+  }
 
-        var order2 = new Order(54321, "store_1234",
-                new BigDecimal("15.00"),
-                OrderType.RESTAURANT,
-                orderItemsRestaurant,
-                //LocalDateTime.now()
-                LocalDateTime.parse("2023-02-21T21:25:01")
-        );
-        var keyValue1 = KeyValue.pair(order1.orderId().toString()
-                , order1);
+  static List<KeyValue<String, Order>> orders() {
 
-        var keyValue2 = KeyValue.pair(order2.orderId().toString()
-                , order2);
+    var orderItems =
+        List.of(
+            new OrderLineItem("Bananas", 2, new BigDecimal("2.00")),
+            new OrderLineItem("Iphone Charger", 1, new BigDecimal("25.00")));
 
+    var orderItemsRestaurant =
+        List.of(
+            new OrderLineItem("Pizza", 2, new BigDecimal("12.00")),
+            new OrderLineItem("Coffee", 1, new BigDecimal("3.00")));
 
-        return List.of(keyValue1, keyValue2);
+    var order1 =
+        new Order(
+            12345,
+            "store_1234",
+            new BigDecimal("27.00"),
+            OrderType.GENERAL,
+            orderItems,
+            // LocalDateTime.now()
+            LocalDateTime.parse("2023-02-21T21:25:01"));
 
-    }
+    var order2 =
+        new Order(
+            54321,
+            "store_1234",
+            new BigDecimal("15.00"),
+            OrderType.RESTAURANT,
+            orderItemsRestaurant,
+            // LocalDateTime.now()
+            LocalDateTime.parse("2023-02-21T21:25:01"));
+    var keyValue1 = KeyValue.pair(order1.orderId().toString(), order1);
 
+    var keyValue2 = KeyValue.pair(order2.orderId().toString(), order2);
+
+    return List.of(keyValue1, keyValue2);
+  }
 }
